45335bfd653841215e2ef128708a448da5257df6
Marko Lisica <markol.lisica@gmail.com>
2025-03-09 19:14:04 +0100

    added patch, get and delete commands to enterprises

--- commands/enterprises.js
+++ commands/enterprises.js
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 import { Command } from 'commander';
 import inquirer from 'inquirer';
 import Table from 'cli-table3';
-
+import fs from 'fs';
 
 export const enterprises = (androidApi, config) => {
     const enterprisesCommand = new Command('enterprises')
@@ -79,8 +79,10 @@ export const enterprises = (androidApi, config) => {
                 }
 
                 enterprises.forEach(enterprise => {
+                    const enterpriseId = enterprise.name.split('/').pop();
+
                     table.push([
-                        enterprise.name,
+                        enterpriseId,
                         enterprise.enterpriseDisplayName,
                     ]);
                 });
@@ -95,6 +97,86 @@ export const enterprises = (androidApi, config) => {
                 console.log("");
                 console.error(chalk.red('Couldn\'t get enterprises:'), error.code + ":", error.message);
             });
+        });
+
+    // enterprises get command
+    enterprisesCommand
+    .command('get')
+    .description('Get enterprise details. Run \'enterprises list\' command to get ID of the enterprise.')
+    .requiredOption('-i, --id <enterprise-id>', 'Specify the ID of the enterprise to get details.')
+    .action((options) => {
+
+        androidApi.enterprises.get({
+            name: `enterprises/${options.id}`,
+        }).then(getEnterpriseDetailsResponse => {
+            const enterpriseDetails = getEnterpriseDetailsResponse.data;
+
+            // Print response
+            console.log("");
+            console.log(chalk.blue(`'${options.id}' enterprise details:`));
+            console.log("");
+            console.log(enterpriseDetails);
+        }).catch(error => {
+            console.log("");
+            console.error(chalk.red('Couldn\'t get enterprise:'), error.code + ": " + error.message);
+        });
+    });
+
+    // enterprises delete command
+    enterprisesCommand
+    .command('delete')
+    .description('Delete enterprise. WARNING: All devices enrolled to this enterprise will be unenrolled.')
+    .requiredOption('-i, --id <enterprise-id>', 'Specify the ID of the enterprise to get details. Run \'enterprises list\' command to get ID of the enterprise.')
+    .action((options) => {
+
+        androidApi.enterprises.delete({
+            name: `enterprises/${options.id}`,
+        }).then(() => {
+
+            // Print response
+            console.log("");
+            console.log(chalk.blue(`'${options.id}' enterprise deleted successfully.`));
+            console.log("");
+        }).catch(error => {
+            console.log("");
+            console.error(chalk.red('Couldn\'t delete enterprise:'), error.code + ": " + error.message);
+        });
+    });
+
+    // enterprises patch command
+    enterprisesCommand
+        .command('patch')
+        .description('Patch an enterprise.')
+        .requiredOption('-f, --file <file-path>', 'Required. Specify path to enterprise JSON file.')
+        .requiredOption('-i, --id <enterprise-id>', 'Required. Specify the ID of the enterprise to patch.')
+        .action((options) => {
+
+            // Get enterprise JSON file from path provided as -f option
+            const enterpriseFilePath = options.file;
+
+            if (fs.existsSync(enterpriseFilePath)) {
+                const enterpriseContent = fs.readFileSync(enterpriseFilePath, 'utf-8');
+                const requestBody = JSON.parse(enterpriseContent);
+
+                androidApi.enterprises.patch({
+                    name: `enterprises/${options.id}`,
+                    requestBody: requestBody
+                }).then(patchEnterpriseResponse => {
+                    const response = patchEnterpriseResponse.data; 
+
+                    console.log("");
+                    console.log(chalk.blue(`'${options.id}' enterprise successfully updated.`));
+                    console.log("")
+                    console.log(response)
+                    console.log("");
+                }).catch(error => {
+                    console.log("");
+                    console.error('Couldn\'t patch enterprise:', error.code + ":", error.message);
+                });
+            } else {
+                console.log(chalk.red('File does not exist. Please provide a valid path.'));
+                return;
+            }
         });
 
     return enterprisesCommand;
